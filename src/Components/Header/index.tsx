89aba9fa7c0793e3931579c739369c775f5c8c96
Leonardo Albuquerque <leoplay2007@hotmail.com>
2024-07-07 15:58:36 -0300

    feat: add svg

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -2,11 +2,18 @@ import React from 'react';
 import './styles.css';
 import { Link } from 'react-router-dom';
 
+import Dog from '../../Assets/dogs.svg?react';
+
 const Header = () => {
   return (
     <header>
       <nav className='container'>
-        <Link to='/'>Home</Link>
+        <Link
+          to='/'
+          aria-label='Dogs - Home'
+        >
+          <Dog />
+        </Link>
         <Link to='/login'>Login / Criar</Link>
       </nav>
     </header>
